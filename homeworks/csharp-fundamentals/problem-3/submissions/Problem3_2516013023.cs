5fdb4eab113470c9930ca2296d5dbaf7f59097fc
Elanur ACAR <2516013023@ogr.cbu.edu.tr>
2026-01-04 18:32:02 +0300

    Update Problem3_2516013023.cs

--- homeworks/csharp-fundamentals/problem-3/submissions/Problem3_2516013023.cs
+++ homeworks/csharp-fundamentals/problem-3/submissions/Problem3_2516013023.cs
@@ -5,103 +5,75 @@ namespace CSharpHomework
 {
     public class Problem3
     {
-      
         public static long Faktoriyel(int n)
         {
-         
-            if (n < 0) return 0; 
-            
-            long sonuc = 1; 
-            
-            
+            long sonuc = 1;
             for (int i = 1; i <= n; i++)
             {
                 sonuc *= i;
             }
-            
             return sonuc;
         }
 
-   
         public static List<int> FibonacciSerisi(int adet)
         {
-            List<int> seri = new List<int>();
-            
-            
-            if (adet <= 0) return seri;
-            
-     
-            seri.Add(0);
-            
-            
-            if (adet == 1) return seri;
-            
-       
-            seri.Add(1);
-            
- 
-            int sayac = 2;
-            
+            List<int> fibonacciListesi = new List<int>();
+            int a = 0;
+            int b = 1;
+            int sayac = 0;
+
             while (sayac < adet)
             {
-           
-                int sonrakiSayi = seri[sayac - 1] + seri[sayac - 2];
-                seri.Add(sonrakiSayi);
+                fibonacciListesi.Add(a);
+                int gecici = a;
+                a = b;
+                b = gecici + b;
                 sayac++;
             }
-            
-            return seri;
+
+            return fibonacciListesi;
         }
 
-      
         public static int BasamakSayisi(int sayi)
         {
-          
-            sayi = Math.Abs(sayi);
-            
             int basamakSayisi = 0;
-            
+            if (sayi < 0)
+            {
+                sayi = Math.Abs(sayi);
+            }
+
             do
             {
-                sayi /= 10; 
+                sayi /= 10;
                 basamakSayisi++;
-            } 
-            while (sayi > 0);
-            
-           
-            
+            } while (sayi > 0);
+
             return basamakSayisi;
         }
 
-        
         public static bool AsalMi(int sayi)
         {
-        
             if (sayi < 2) return false;
-            
-          
+
+            for (int i = 2; i <= Math.Sqrt(sayi); i++)
             {
                 if (sayi % i == 0)
                 {
-                    return false; 
+                    return false;
                 }
             }
-            
+
             return true;
         }
 
         public static int SayilarinToplami(int n)
         {
             int toplam = 0;
-            
             for (int i = 1; i <= n; i++)
             {
                 toplam += i;
             }
-            
             return toplam;
-            
-           
         }
     }
 }
